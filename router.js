87e81d521b922f5549731ef6f45fe87a3d0a617a
Mintu Krishnan S <mintu.krish999@hotmail.com>
2021-02-20 16:45:10 +0530

    completed login and logout

--- router.js
+++ router.js
@@ -6,24 +6,38 @@ const credential = {
   password: "admin",
 };
 
-router.post("/", (req, res) => {
+router.post("/login", (req, res) => {
   if (
     req.body.email == credential.email &&
     req.body.password == credential.password
   ) {
     req.session.user = req.body.email;
-    res.redirect("login/dashboard");
+    res.redirect("dashboard");
   } else {
     res.end("Invalid Username");
   }
 });
 
-router.get("/dashboard", (res, req) => {
+router.get("/dashboard", (req, res) => {
   if (req.session.user) {
-    res.render("dashboard", { user: req.session.user });
+    res.render("dashboard", {
+      user: req.session.user,
+      title: req.session.user,
+    });
   } else {
     res.send("unauthorized User");
   }
+});
+
+router.get("/logout", (req, res) => {
+  req.session.destroy(function (err) {
+    if (err) {
+      console.log(err);
+      res.send("Error");
+    } else {
+      res.render("index", { title: "Express", logout: "Logout Successfully" });
+    }
+  });
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,15 +6,16 @@ const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 
-const port = process.env.PORT || 3000;
-
 const router = require("./router");
+
+const port = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
 
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: true }));
-app.use("/", express.static(path.join(__dirname, "public")));
+app.use("/static", express.static(path.join(__dirname, "public")));
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 app.use(
   session({
     secret: uuidv4(),
@@ -23,7 +24,7 @@ app.use(
   })
 );
 
-app.use("/login", router);
+app.use("/route", router);
 
 app.get("/", (req, res) => {
   res.render("index", { title: "Login Systems" });
