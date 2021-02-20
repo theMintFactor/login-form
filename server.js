f071cebcb4fdf7753cf578344d2e55a3fb3e7ec5
Mintu Krishnan S <mintu.krish999@hotmail.com>
2021-02-20 11:44:15 +0530

    added styles and bg image to login form

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
 const port = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
+
+// load static assets
+app.use("/", express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.render("index", { title: "Login Systems" });
